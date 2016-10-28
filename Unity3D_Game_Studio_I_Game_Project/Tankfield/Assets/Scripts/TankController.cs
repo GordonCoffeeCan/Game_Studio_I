3e670a8713958fa480d5a35ae21333d1543fb306
GordonLee6170 <gordonlee@172-16-13-38.DYNAPOOL.NYU.EDU>
2016-10-28 10:37:13 -0400

    Mac

---
+++ Unity3D_Game_Studio_I_Game_Project/Tankfield/Assets/Scripts/TankController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankController : MonoBehaviour {
+	public float speed = 5;
+
+	private Transform myTank;
+	private CharacterController myController;
+
+	private Vector3 moveDirection;
+
+	// Use this for initialization
+	void Start () {
+		myTank = this.transform;
+		myController = this.GetComponent<CharacterController> ();
+		moveDirection = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveDirection = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+		moveDirection = transform.TransformDirection (moveDirection);
+		moveDirection *= speed;
+		myController.Move (moveDirection * Time.deltaTime);
+		
+	}
+}
